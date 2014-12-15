f95fba60585b3b4641dba372a87e909a5b5a8932
James Taylor <jamestaylr@gmail.com>
2014-12-14 22:04:53 -0500

    Added test code to turn servos from GPIO

--- src/main.py
+++ src/main.py
@@ -1,4 +1,34 @@
-def main():
-    print("Beginning SailBOT autonomous navigation routines\n");
+import time
 
-if __name__ == "__main__": main()
+def set(property, value):
+    try:
+        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f.write(value)
+        f.close()    
+    except:
+        print("Error writing to: " + property + " value: " + value)
+
+def setServo(angle):
+    set("servo", str(angle))
+    
+def configureServos():
+    set("delayed", "0")
+    set("mode", "servo")
+    set("servo_max", "180")
+    set("active", "1")
+
+## ----------------------------------------------------------
+
+DELAY_PERIOD = 0.01
+
+print("Beginning SailBOT autonomous navigation routines\n");
+configureServos()
+
+while True:
+    for angle in range(0, 180):
+        setServo(angle)
+        time.sleep(DELAY_PERIOD)
+    for angle in range(0, 180):
+        setServo(180 - angle)
+        time.sleep(DELAY_PERIOD)
+

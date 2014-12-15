6a76640fc814e636249fe2d986bf9ea02ac32a7a
James Taylor <jamestaylr@gmail.com>
2014-12-15 14:08:02 -0500

    Create boat marker as a custom Google maps element

--- src/server/js/map.js
+++ src/server/js/map.js
@@ -58,14 +58,43 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById('map-canvas'),
 			mapOptions);
 
-	var marker = new google.maps.Marker({
-		position : {
-			lat : 37.047764,
-			lng : -79.639314
-		},
-		map : map,
-		title : 'Hello World!'
-	});
+	overlay = new CustomMarker(map.getCenter(), map);
 
 }
+
+function CustomMarker(latlng, map) {
+	this.latlng_ = latlng;
+	this.setMap(map);
+}
+
+CustomMarker.prototype = new google.maps.OverlayView();
+
+CustomMarker.prototype.draw = function() {
+
+	var div = this.div_;
+	if (!div) {
+		// create the overlay element
+		div = this.div_ = document.createElement('DIV');
+		div.style.border = "none";
+		div.style.position = "absolute";
+		div.style.paddingLeft = "0px";
+		div.style.cursor = 'pointer';
+
+		var d = document.createElement("div");
+		d.className = 'pulse'
+		div.appendChild(d);
+
+		// add the overlay to the DOM
+		var panes = this.getPanes();
+		panes.overlayImage.appendChild(div);
+	}
+
+	// position the overlay
+	var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
+	if (point) {
+		div.style.left = point.x + 'px';
+		div.style.top = point.y + 'px';
+	}
+};
+
 google.maps.event.addDomListener(window, 'load', initialize);

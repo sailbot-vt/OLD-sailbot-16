62619a55614988ef3ca8ed2ae5de248712b1590f
James Taylor <jamestaylr@gmail.com>
2014-12-15 19:56:54 -0500

    Minor stylistic and formatting changes

--- src/server/js/map.js
+++ src/server/js/map.js
@@ -1,65 +1,65 @@
 function initialize() {
 	var mapOptions = {
-			center : {
-				lat : 37.047764,
-				lng : -79.639314
-			},
-			zoom : 14,
+		center : {
+			lat : 37.047764,
+			lng : -79.639314
+		},
+		zoom : 14,
 
-			panControl : true,
-			zoomControl : true,
-			mapTypeControl : false,
-			scaleControl : true,
-			streetViewControl : false,
-			overviewMapControl : false,
+		panControl : true,
+		zoomControl : true,
+		mapTypeControl : false,
+		scaleControl : true,
+		streetViewControl : false,
+		overviewMapControl : false,
 
-			styles : [ {
-				"featureType" : "water",
-				"stylers" : [ {
-					"color" : "#46bcec"
-				}, {
-					"visibility" : "on"
-				} ]
+		styles : [ {
+			"featureType" : "water",
+			"stylers" : [ {
+				"color" : "#46bcec"
 			}, {
-				"featureType" : "landscape",
-				"stylers" : [ {
-					"color" : "#f2f2f2"
-				} ]
-			}, {
-				"featureType" : "road",
-				"stylers" : [ {
-					"saturation" : -100
-				}, {
-					"lightness" : 45
-				} ]
-			}, {
-				"featureType" : "road.highway",
-				"stylers" : [ {
-					"visibility" : "simplified"
-				} ]
-			}, {
-				"featureType" : "road.arterial",
-				"elementType" : "labels.icon",
-				"stylers" : [ {
-					"visibility" : "off"
-				} ]
-			}, {
-				"featureType" : "administrative",
-				"elementType" : "labels.text.fill",
-				"stylers" : [ {
-					"color" : "#444444"
-				} ]
-			}, {
-				"featureType" : "transit",
-				"stylers" : [ {
-					"visibility" : "off"
-				} ]
-			}, {
-				"featureType" : "poi",
-				"stylers" : [ {
-					"visibility" : "off"
-				} ]
+				"visibility" : "on"
 			} ]
+		}, {
+			"featureType" : "landscape",
+			"stylers" : [ {
+				"color" : "#f2f2f2"
+			} ]
+		}, {
+			"featureType" : "road",
+			"stylers" : [ {
+				"saturation" : -100
+			}, {
+				"lightness" : 45
+			} ]
+		}, {
+			"featureType" : "road.highway",
+			"stylers" : [ {
+				"visibility" : "simplified"
+			} ]
+		}, {
+			"featureType" : "road.arterial",
+			"elementType" : "labels.icon",
+			"stylers" : [ {
+				"visibility" : "off"
+			} ]
+		}, {
+			"featureType" : "administrative",
+			"elementType" : "labels.text.fill",
+			"stylers" : [ {
+				"color" : "#444444"
+			} ]
+		}, {
+			"featureType" : "transit",
+			"stylers" : [ {
+				"visibility" : "off"
+			} ]
+		}, {
+			"featureType" : "poi",
+			"stylers" : [ {
+				"visibility" : "off"
+			} ]
+		} ]
 	};
 
 	var map = new google.maps.Map(document.getElementById('map-canvas'),

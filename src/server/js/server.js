3e7d42d73f0ea206f120a275b2e7d3c37ce5c5fa
James Taylor <jamestaylr@gmail.com>
2014-12-21 19:33:06 -0500

    Localhost IP bug fixed; tested parsing methods

--- src/server/js/server.js
+++ src/server/js/server.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 
-	socket = new WebSocket('ws://127.0.01:4041/ws');
+	socket = new WebSocket('ws://127.0.0.1:4041/ws');
 
 	socket.onmessage = function(s) {
-		alert('Received: ' + s);
-		var request = JSON.parse(s);
-		var data = request[0];
+		alert('Received data!');
+		
+		//TODO: get server side working to implement this feature
 		
 		document.getElementById("timestamp").innerHTML = data.timestamp;
 		

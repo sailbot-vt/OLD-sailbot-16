eaf947576c1c31ce5c3251e018e81dd0496c7fcf
James Taylor <jamestaylr@gmail.com>
2014-12-22 14:37:55 -0500

    Defines a callback function to send global data
    A callback function breaks the server IO loop at a given time interval
    to run a function inside the code. A global method `wsSend(message)`
    can now write to the socket.

--- src/server.py
+++ src/server.py
@@ -3,27 +3,48 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
+wss = []
+
 class WSHandler(tornado.websocket.WebSocketHandler):
     
     def check_origin(self, origin):
         return True
     
     def open(self):
-        print ("new connection");
-        self.write_message("Hello World")
+        print 'New connection established.'
+        if self not in wss:
+            wss.append(self)
       
     def on_message(self, message):
-        print ('message received %s' % message);
+        print 'Received message: %s' % message
  
     def on_close(self):
-      print ('connection closed')
- 
+        print 'Connection closed.'
+        if self in wss:
+            wss.remove(self)
+
+def wsSend(message):
+    for ws in wss:
+        ws.write_message(message);
  
 application = tornado.web.Application([
     (r'/ws', WSHandler),
 ])
+
+def schudule_func():
+    # called periodically
+    wsSend("Ping!")
  
 if __name__ == "__main__":
+    print "Starting server."
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(4041)
-    tornado.ioloop.IOLoop.instance().start()
+    
+    # creates a periodic callback function
+    interval_ms = 1000
+    main_loop = tornado.ioloop.IOLoop.instance()
+    sched = tornado.ioloop.PeriodicCallback(schudule_func(), interval_ms, io_loop = main_loop)
+    
+    # starts the callback and the main IO loop
+    sched.start()
+    main_loop.start()

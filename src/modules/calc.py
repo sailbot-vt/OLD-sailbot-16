0553dc7a6adc74095c48459a43dec73b2250d920
James Taylor <jamestaylr@gmail.com>
2015-01-04 08:03:04 -0500

    Rewrote `point_proximity()`
    Implemented new algorithm to increase accuracy and computational
    efficiency.

--- src/modules/calc.py
+++ src/modules/calc.py
@@ -32,17 +32,16 @@ def direction_to_point(current, target):
     return angle
 
 def point_proximity(current, target):
-    dist_lat = 111132.954 - 559.822 * math.cos(2 * current.latitude) \
-        + 1.1175 * math.cos(4 * current.latitude)
-    dist_lon = math.pi / 180 * 6367449 * math.cos(current.latitude)
-
-    lat_meter = math.fabs(math.fabs(target.latitude) - math.fabs(current.latitude)) * dist_lat
-    lon_meter = math.fabs(math.fabs(target.longitude) - math.fabs(current.longitude)) * dist_lon
+    a = math.sin(math.radians(current.latitude))
+    b = math.sin(math.radians(target.latitude))
+    c = math.cos(math.radians(current.latitude))
+    d = math.cos(math.radians(target.latitude))
     
-    distance = math.sqrt(math.pow(lat_meter, 2) + math.pow(lon_meter, 2))
+    e = a * b + c * d * math.cos((math.radians(target.longitude - current.longitude)))
+    f = math.acos(e)
+    distance = f * 6371 * 1000
     
-    return (distance <= 10) # point_proximity_radius can be changed
-
+    return (distance <= 5) # point_proximity_radius can be changed
 
 
             

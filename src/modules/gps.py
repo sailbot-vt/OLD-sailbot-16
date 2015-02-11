5f06a6648d54cf33735d20f1a0d0e47a587c1463
jamestaylr <jamestaylr@gmail.com>
2015-02-10 19:43:46 -0500

    Updated GPS module and included Pickle framework

--- src/modules/gps.py
+++ src/modules/gps.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python
 
-import os
-import logging
-try:
-    from gps import *
-except ImportError:
-    pass
-from time import *
+from gps import *
+
 import time
 import threading
+import pickle
 
 gpsd = None  # seting the global variable
 
@@ -24,7 +20,29 @@ class GPSPoller(threading.Thread):
     def run(self):
         global gpsd
         while gpsp.running:
-            gpsd.next()  # continuously loop and grab each set of gpsd info to clear the buffer
-
-
+        
+            gpsd_data = {'latitude': gpsd.fix.latitude,
+                         'longitude': gpsd.fix.latitude,
+                         'timestamp': gpsd.fix.time,
+                         'heading': gpsd.attitude.heading,
+                         'speed': gpsd.fix.speed, 'roll': gpsd.attitude.roll,
+                         'pitch': gpsd.attitude.pitch,
+                         'yaw': gpsd.attitude.yaw}
             
+            for key in gpsd_data:
+                print('%s: %d' % key, gpsd_data[key])
+            
+            pickle.dump(gpsd_data, open("gpsd_data.p", "wb"))
+            
+            
+            # continuously loop and grab each set of gpsd info to clear the buffer
+            gpsd.next()
+            time.sleep(0.5)
+
+try:
+    gpsd_thread = GPSPoller()
+    gpsd_thread.start()
+except NameError:
+    print("GPS not configured properly on this device!")
+    import sys
+    sys.exit(1)

3d4717d575e21889cad22c4e58f707104951fbd0
jamestaylr <jamestaylr@gmail.com>
2015-02-23 01:03:39 -0500

    Added unimplemented `ConsoleFormatter`; changed logging configuration
    The console log now supports Unicode color characters. This feature has
    been unimplemented, but will hopefully be fully functional in a future
    release.

--- src/modules/log.py
+++ src/modules/log.py
@@ -1,7 +1,7 @@
 import logging
 import modules.utils
 
-class WebSocketLogger(logging.StreamHandler):
+class WebSocketLogger(logging.Handler):
 
     """
     A handler class which allows the cursor to stay on
@@ -21,5 +21,26 @@ class WebSocketLogger(logging.StreamHandler):
             print('The server thread has not been created yet. Dropping log output.')
         except:
             self.handleError(record)
+            
+class ConsoleFormatter(logging.Formatter):
+    def format(self, record):
+        if record.levelno  == logging.WARNING:
+            # display yellow text
+            record.msg = '\033[93m\033[1m%s\033[0m\033[39m' % (record.msg)
+        elif record.levelno == logging.ERROR:
+            # display red text
+            record.msg = '\033[31m\033[1m%s\033[0m\033[39m' % (record.msg)
+        elif record.levelno == logging.INFO:
+            # display blue text
+            record.msg = '\033[94m\033[1m%s\033[0m\033[39m' % (record.msg)
+        elif record.levelno == logging.CRITICAL:
+            # display bold red text
+            record.msg = '\033[31m\033[1m%s\033[0m\033[39m' % (record.msg)
+        elif record.levelno == logging.DEBUG:
+            # display green text
+            record.msg = '\033[92m\033[1m%s\033[0m\033[39m' % (record.msg)
+            
+        return super(ConsoleFormatter , self).format(record)
+
 
         

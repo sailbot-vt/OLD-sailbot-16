fb39212f3e440abeac30a0d3896f57dd60d0a860
jamestaylr <jamestaylr@gmail.com>
2015-03-21 15:48:23 -0400

    Minor changes to build and launch scripts

--- src/launch.sh
+++ src/launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo '\033[94m\033[1mStarting SailBOT...\033[0m\033[39m'
+echo '\033[36m\033[1mStarting SailBOT...\033[0m\033[39m'
 
-echo '\033[94m\033[1mConfiguring GPSD...\033[0m\033[39m'
+echo '\033[36m\033[1mConfiguring GPSD...\033[0m\033[39m'
 sudo killall gpsd
 sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 
-echo '\033[94m\033[1mCreating GPSD TCP Socket...\033[0m\033[39m'
+echo '\033[36m\033[1mCreating GPSD TCP Socket...\033[0m\033[39m'
 sudo python "modules/pgps.py" &
 
-echo '\033[94m\033[1mLaunching main program...\033[0m\033[39m'
+echo '\033[36m\033[1mLaunching main program...\033[0m\033[39m'
 sudo python3 main.py

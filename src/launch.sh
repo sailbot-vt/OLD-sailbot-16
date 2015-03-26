3c46d5dc0a024c436a8a314e193057bb81ed3be8
James Taylor <jamestaylr@gmail.com>
2015-03-26 01:48:05 -0400

    Added wind sensor TCP socket process to launch script

--- src/launch.sh
+++ src/launch.sh
@@ -9,5 +9,8 @@ sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 echo '\033[36m\033[1mCreating GPSD TCP Socket...\033[0m\033[39m'
 sudo python "modules/pgps.py" &
 
+echo '\033[36m\033[1mCreating Wind Sensor TCP Socket...\033[0m\033[39m'
+sudo python "modules/wind_sensor.py" &
+
 echo '\033[36m\033[1mLaunching main program...\033[0m\033[39m'
 sudo python3 main.py

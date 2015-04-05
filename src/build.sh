59bd10a58e5f41f40a565284a44efa326d62e1f0
James Taylor <jamestaylr@gmail.com>
2015-04-05 10:46:37 -0400

    Condensed build script to use Pip
    Pip, the Python package manager, now installs the Tornado Web Server,
    eliminating the need to keep the `wget` request link updated.

--- src/build.sh
+++ src/build.sh
@@ -6,24 +6,13 @@ echo '\033[36m\033[1mUpdating Raspberry Pi...\033[0m\033[39m'
 sudo apt-get update
 
 echo '\033[36m\033[1mInstalling Python 3, GPSD, and I2C Dependencies...\033[0m\033[39m'
-sudo apt-get install build-essential python3-dev gpsd gpsd-clients python-gps i2c-tools python-smbus
+sudo apt-get install build-essential python3-dev gpsd gpsd-clients python-gps i2c-tools python-smbus python-pip
 
 echo '\033[36m\033[1mRefreshing packages...\033[0m\033[39m'
 sudo apt-get update
 
-echo '\033[36m\033[1mFetching Tornado Web Server...\033[0m\033[39m'
-cd ~/
-wget https://pypi.python.org/packages/source/t/tornado/tornado-4.1.tar.gz
-tar xvzf tornado-4.1.tar.gz
-cd tornado-4.1
-echo '\033[36m\033[1mBuilding Tornado Web Server...\033[0m\033[39m'
-python3 setup.py build
-sudo python3 setup.py install
-
-echo '\033[36m\033[1mCleaning up installation...\033[0m\033[39m'
-cd ..
-rm -r tornado-4.1/
-rm tornado-4.1.tar.gz
+echo '\033[36m\033[1mSetting up Tornado Web Server...\033[0m\033[39m'
+sudo pip install tornado
 
 echo '\033[36m\033[1mPulling project from repository...\033[0m\033[39m'
 cd ~/

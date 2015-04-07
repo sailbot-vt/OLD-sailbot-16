1ac207250a72ec99403ac95628b773844d78f1b9
jamestaylr <jamestaylr@gmail.com>
2015-04-07 09:30:00 -0400

    Condensed build script to include `print_message` function

--- src/build.sh
+++ src/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-echo '\033[36m\033[1mStarting SailBOT Build Script...\033[0m\033[39m'
+print_message() {
+	echo '\033[36m\033[1m'$1'\033[0m\033[39m'
+}
 
-echo '\033[36m\033[1mUpdating Raspberry Pi...\033[0m\033[39m'
+print_message 'Starting SailBOT Build Script...'
+
+print_message 'Updating Raspberry Pi...'
 sudo apt-get update
 
-echo '\033[36m\033[1mInstalling Python 3, GPSD, and I2C Dependencies...\033[0m\033[39m'
+print_message 'Installing Python 3, GPSD, and I2C Dependencies...'
 sudo apt-get install build-essential python3-dev gpsd gpsd-clients python-gps i2c-tools python-smbus python-pip
 
-echo '\033[36m\033[1mRefreshing packages...\033[0m\033[39m'
+print_message 'Refreshing packages...'
 sudo apt-get update
 
-echo '\033[36m\033[1mSetting up Tornado Web Server...\033[0m\033[39m'
+print_message 'Setting up Tornado Web Server...'
 sudo pip install tornado
 
-echo '\033[36m\033[1mPulling project from repository...\033[0m\033[39m'
+print_message 'Pulling project from repository...'
 cd ~/
 mkdir SailBOT
 cd SailBOT

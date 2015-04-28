3805abc50c3c0ed2fc3915095f953bd63ba2f765
James Taylor <jamestaylr@gmail.com>
2015-04-28 18:42:10 -0400

    Wind sensor handler now exists gracefully

--- src/modules/wind_sensor.py
+++ src/modules/wind_sensor.py
@@ -21,10 +21,10 @@ import json
 import threading
 
 
-wind_sensor = None  # global wind_sensor variable
+wind_sensor = None  # Global wind_sensor variable
 angle = 0.0
 
-# define the socket parameters
+# Define the socket parameters
 
 HOST = ''
 PORT = 8894
@@ -32,7 +32,7 @@ PORT = 8894
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 
-# bind socket to local host and port
+# Bind socket to local host and port
 try:
     connection.bind((HOST, PORT))
 except socket.error, msg:
@@ -55,7 +55,6 @@ class WindSensor(threading.Thread):
     def switch_bits(self, input):
 
         # Flips the bits of the input and returns the result
-
         return (input >> 8) + ((input & 0x00ff) << 8)
 
     def set_data_channel(self, input, bus):
@@ -65,14 +64,12 @@ class WindSensor(threading.Thread):
     def read_data(self, bus):
 
         # Read data from the bus
-
         time.sleep(0.01)
         return self.switch_bits(bus.read_word_data(self.address, 0)) >> 4
 
     def setup(self, input, bus):
 
         # Write values to the registers
-
         bus.write_word_data(self.address, 0x02, self.switch_bits(input << 4))
         time.sleep(0.01)
         bus.write_word_data(self.address, 0x03, self.switch_bits(0x7fff))
@@ -81,7 +78,6 @@ class WindSensor(threading.Thread):
     def calculate_angle(self, input):
 
         # Alter the maximum and minimum values during calibration
-
         if input > self.max:
             self.max = input
 
@@ -91,7 +87,6 @@ class WindSensor(threading.Thread):
         a = self.max - self.min
 
         # Return the measured value as a proportionally scaled angle
-
         return (input - self.min) * (360.0 / a)
 
     def run(self):
@@ -106,27 +101,25 @@ class WindSensor(threading.Thread):
                 angle = self.calculate_angle(self.read_data(bus))
                 time.sleep(0.1)
         except IOError:
-            generate_error('IO Error: device cannot be read, check your wiring')
+            generate_error('IO Error: device cannot be read, check your wiring or run as root')
 
-# create and start the wind_sensor thread
-
+# Create and start the wind_sensor thread
 wind_sensor = WindSensor()
+wind_sensor.daemon = True # Needed to make the thread shutdown correctly
 wind_sensor.start()
 
 # Start listening on socket
-
 connection.listen(10)
 
-# function for handling connections; will be used to create threads
+# Function for handling connections; will be used to create threads
 
 def clientthread(conn):
 
-    # infinite loop so that function do not terminate and thread do not end
+    # Infinite loop so that function do not terminate and thread do not end
 
     while True:
 
-        # receive data from the client
-
+        # Receive data from the client
         data = conn.recv(1024)
         if not data:
             break
@@ -134,24 +127,24 @@ def clientthread(conn):
         conn.sendall(json.dumps(angle).encode('utf-8'))
 
     # close the connection if the client if the client and server connection is interfered
-
     conn.close()
 
 
-# main loop to keep the server process going
-
+# Main loop to keep the server process going
 while True:
 
-    # wait to accept a connection in a blocking call
+    try:
+        # Wait to accept a connection in a blocking call
+        (conn, addr) = connection.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
-    (conn, addr) = connection.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        # Start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function
+        start_new_thread(clientthread, (conn, ))
 
-    # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function
-
-    start_new_thread(clientthread, (conn, ))
-
-connection.close()
+    except KeyboardInterrupt, socket.error:
+        connection.shutdown(socket.SHUT_RDWR)
+        connection.close()
+        break
 
 
             

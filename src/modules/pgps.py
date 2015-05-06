6d841ae9474936ad50650b11f6b3a579114379ed
James Taylor <jamestaylr@gmail.com>
2015-05-06 15:59:07 -0400

    Fixed shutdown functions in GPS process; added debug capabilities
    The GPS thread now gracefully exits when the `KeyboardInterrupt` is
    triggered. Debug commands have been added throughout. STDOUT statements
    comply with the standard format.

--- src/modules/pgps.py
+++ src/modules/pgps.py
@@ -9,11 +9,14 @@ import threading
 
 def generate_error(message):
     print '\033[31m\033[1m%s\033[0m\033[39m' % message
+
+def generate_debug(message):
+    print '\033[32m\033[1m%s\033[0m\033[39m' % message
     
 try:
     from gps import *
 except ImportError:
-    generate_error('GPS not configured properly!')
+    generate_error('[GPS Socket]: GPS not configured properly!')
     sys.exit(1)
 
 gpsd = None  # Global GPSD variable
@@ -30,31 +33,43 @@ connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
     connection.bind((HOST, PORT))
 except socket.error, msg:
-    generate_error('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' \
+    generate_error('[GPS Socket]: Bind failed. Error Code: ' + str(msg[0]) + ' Message ' \
         + msg[1])
     sys.exit()
 
-print 'GPS socket bind complete!'
+print '[GPS Socket]: GPS socket bind complete!'
 
 # GPSD polling class definitions
 class GPSPoller(threading.Thread):
 
     def __init__(self):
+        generate_debug('[GPS Socket]: Setting up GPS')
         threading.Thread.__init__(self)
         global gpsd  # Bring it in scope
         gpsd = gps(mode=WATCH_ENABLE)  # Starting the stream of info
         self.current_value = None
         self.running = True  # Setting the thread running to true
+        generate_debug('[GPS Socket]: Finished setting up GPS')
 
     def run(self):
         global gpsd
+        generate_debug('[GPS Socket]: Entered thread')
         while gpsp.running:
             gpsd.next()  # This will continue to loop and grab each set of GPSD info to clear the buffer
+            time.sleep(0.1)
 
 # Create and start the GPSD thread
-gpsp = GPSPoller()
-gpsp.daemon = True # Needed to make the thread shutdown correctly
-gpsp.start()
+generate_debug('[GPS Socket]: Starting the GPS thread')
+try:
+    gpsp = GPSPoller()
+    gpsp.daemon = True # Needed to make the thread shutdown correctly
+    gpsp.start()
+
+except socket.error:
+    generate_error('[GPS Socket]: Could not connect to the GPS! Make sure everything is connected correctly.')
+    sys.exit()
+
+generate_debug('[GPS Socket]: Started TCP listerner')
 
 # Start listening on socket
 connection.listen(10)
@@ -80,6 +95,8 @@ def clientthread(conn):
 
 def update_gpsd_data():
     try:
+        generate_debug('[GPS Socket]: GPS data requested')
+
         # Tries to pull in all the critical GPS data
         gpsd_data.clear()
         gpsd_data.update({
@@ -107,12 +124,13 @@ while True:
     try:
         # Wait to accept a connection in a blocking call
         (conn, addr) = connection.accept()
-        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        print '[GPS Socket]: Connected with ' + addr[0] + ':' + str(addr[1])
 
         # Start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function
         start_new_thread(clientthread, (conn, ))
 
     except KeyboardInterrupt, socket.error:
+        generate_debug('[GPS Socket]: Shutting down GPS!')
         connection.shutdown(socket.SHUT_RDWR)
         connection.close()
         break

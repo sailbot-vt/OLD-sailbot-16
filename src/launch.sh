0f47632941683b9c130ceb94004df939c0e385e4
James Taylor <jamestaylr@gmail.com>
2015-05-23 20:22:58 -0400

    Improved privilege control in launch script

--- src/launch.sh
+++ src/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 print_message() {
 	echo '\033[36m\033[1m'$1'\033[0m\033[39m'
 }
@@ -10,10 +12,10 @@ print_message 'Creating GPSD TCP Socket...'
 ./modules/pgps 8907 &
 
 print_message 'Creating Wind Sensor TCP Socket...'
-sudo python "modules/wind_sensor.py" &
+python "modules/wind_sensor.py" &
 
 print_message 'Creating Rudder TCP Socket...'
-sudo python "modules/servo.py" 9107 18 1.5 1.2 &
+python "modules/servo.py" 9107 18 1.5 1.2 &
 
 print_message 'Launching main program...'
 sudo python3 main.py

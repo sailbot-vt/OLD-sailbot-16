e27a69d9e8759987817be6a6c2c44bc91d1d4938
James Taylor <jamestaylr@gmail.com>
2015-06-10 00:00:29 -0400

    Minor edits to helper method calculations

--- src/modules/calc.py
+++ src/modules/calc.py
@@ -16,7 +16,7 @@ def direction_to_point(current_point, target_point):
     y = math.sin(d) * math.cos(b)
     x = math.cos(a) * math.sin(b) - math.sin(a) * math.cos(b) * math.cos(d)
     
-    return 360 - ((math.degrees(math.atan2(y, x)) + 360) % 360)
+    return (math.degrees(math.atan2(y, x)) + 360) % 360
 
 def get_heading_angle(heading, current_point, target_point):
     angle = direction_to_point(current_point, target_point)
@@ -35,5 +35,13 @@ def point_proximity(current_point, target_point):
     
     return (distance <= point_proximity_radius)
 
+def distance(point1, point2):
+    a = math.sin(math.radians(point1.latitude))
+    b = math.sin(math.radians(point2.latitude))
+    c = math.cos(math.radians(point1.latitude))
+    d = math.cos(math.radians(point2.latitude))
 
-            
+    e = a * b + c * d * math.cos((math.radians(point2.longitude - point1.longitude)))
+    f = math.acos(e)
+
+    return f * 6371 * 1000
